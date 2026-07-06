0b0c525d171d7264d88d4ed62f81de69799d6c5e
melxusgid <up_n_oot@protonmail.com>
2026-07-05 22:09:49 -0700

    Initial Commit

---
+++ Keep/KeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepApp.swift
+//  Keep
+//
+//  Created by RotaryPhone on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct KeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
